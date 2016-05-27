56e012d92bebceb3baaeabe73ad74c98447bacf2
Markus Barthel <markus.barthel.mb@gmail.com>
2016-05-27 22:34:10 +0200

    fixed models, add model for book entry type

--- bibbutler_web/admin.py
+++ bibbutler_web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models.entrytypes import EntryType_Book, EntryType_Online
+
 # Register your models here.
+admin.site.register(EntryType_Book)
+admin.site.register(EntryType_Online)
--- bibbutler_web/models/entrytypes.py
+++ bibbutler_web/models/entrytypes.py
@@ -1,8 +1,9 @@
 from abc import ABC
-from datetime import date
+from datetime import date, datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.dates import MONTHS
+from django.utils import timezone
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator
 
@@ -10,8 +11,8 @@ from django.core.validators import MaxValueValidator
 class EntryType(models.Model):
     title = models.CharField(max_length=60)
     subtitle = models.CharField(max_length=60)
-    urldate = models.DateField('url date',blank=True, default=date.today())
-    url = models.URLField(blank=True)
+    urldate = models.DateField('url date',blank=True, default=timezone.now)
+    url = models.URLField(blank=True, null=True)
     addendum = models.TextField(blank=True, max_length=200)
     note = models.TextField(blank=True, max_length=200)
     language = models.CharField(blank=True, max_length=15)
@@ -25,8 +26,8 @@ class EntryType(models.Model):
     pubstate = models.CharField(blank=True, max_length=5, choices=PUBSTATE_CHOICES, default=None)
 
     # required once of them
-    date = models.DateField(blank=True)
-    year = models.PositiveSmallIntegerField(blank=True, validators=[MaxValueValidator(date.today().year,message='year is in future')])
+    date = models.DateField(blank=True, null=True)
+    year = models.PositiveSmallIntegerField(blank=True, null=True, validators=[MaxValueValidator(datetime.today().year, message='year is in future')])
     def clean(self):
         if not self.date and not self.year:
             raise ValidationError(_('one of the both fields date and year have to be set.'), code='requirements not set')
@@ -38,8 +39,9 @@ class EntryType(models.Model):
 
 class AuthorOrEditorRequired(models.Model):
     # required once of them
-    author = models.CharField(blank=True, max_length=100)
-    editor = models.URLField(blank=True, max_length=100)
+    author = models.CharField(blank=True, max_length=150)
+    editor = models.CharField(blank=True, max_length=150)
+    # editortype
     def clean(self):
         if not self.author and not self.editor:
             raise ValidationError(_('one of the both fields author and editor have to be set.'), code='requirements not set')
@@ -48,21 +50,37 @@ class AuthorOrEditorRequired(models.Model):
     class Meta:
         abstract = True
 
+# class EditorXAndType(models.Model):
+#     raise NotImplementedError
 
-class EntryType_Online(EntryType, AuthorOrEditorRequired):
-    titleaddon = models.URLField(blank=True, max_length=60)
-    version = models.CharField(blank=True, max_length=20)
-    organization = models.CharField(blank=True, max_length=60)
-    month = models.PositiveSmallIntegerField(blank=True, choices=MONTHS.items())
+
+class EntryType_Book(EntryType):
+    author = models.CharField(max_length=150)
+    editor = models.CharField(blank=True, max_length=150)
+    publisher = models.CharField(blank=True, max_length=100)
+    location = models.CharField(blank=True, max_length=100)
+    isbn = models.CharField(blank=True, max_length=16)
+    titleaddon = models.URLField(blank=True, null=True, max_length=60)
 
     def clean(self):
         super().clean()
 
-# make url field required on entry type online
+
+class EntryType_Online(EntryType, AuthorOrEditorRequired):
+    titleaddon = models.URLField(blank=True, null=True, max_length=60)
+    version = models.CharField(blank=True, max_length=20)
+    organization = models.CharField(blank=True, max_length=60)
+    month = models.PositiveSmallIntegerField(blank=True, null=True, choices=MONTHS.items())
+
+    def clean(self):
+        super().clean()
+# make url field required on online entry type
 EntryType_Online._meta.get_field('url').blank = False
+EntryType_Online._meta.get_field('url').null = False
 
 
 
 entry_types = {
-    'article' : EntryType_Online
+    'article' : EntryType_Online,
+    'book' : EntryType_Book
 }
